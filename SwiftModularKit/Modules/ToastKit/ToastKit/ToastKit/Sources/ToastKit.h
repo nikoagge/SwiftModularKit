d2a9cda4e349ffcb862895b1cf83400c089097e9
Nikos Aggelidis <nikoagge@gmail.com>
2025-04-13 11:15:19 +0300

    chore: remove nested git repo and clean up ToastKit structure

---
+++ SwiftModularKit/Modules/ToastKit/ToastKit/ToastKit/Sources/ToastKit.h
@@ -0,0 +1,18 @@
+//
+//  ToastKit.h
+//  ToastKit
+//
+//  Created by Nikos Aggelidis on 12/4/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ToastKit.
+FOUNDATION_EXPORT double ToastKitVersionNumber;
+
+//! Project version string for ToastKit.
+FOUNDATION_EXPORT const unsigned char ToastKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToastKit/PublicHeader.h>
+
+
---
+++ SwiftModularKit/Modules/ToastKit/ToastKit/ToastKitDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToastKitDemoApp
+//
+//  Created by Nikos Aggelidis on 13/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
